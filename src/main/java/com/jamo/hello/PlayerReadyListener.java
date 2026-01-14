ba912afade2a2ec5f22ff96eb71a3196ea28f44b
uwutaube <yusufxtd@gmail.com>
2026-01-14 16:32:47 +0000

    first commit

---
+++ src/main/java/com/jamo/hello/PlayerReadyListener.java
@@ -0,0 +1,16 @@
+package com.jamo.hello;
+
+import com.hypixel.hytale.component.ArchetypeChunk;
+import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.component.query.Query;
+import com.hypixel.hytale.component.system.EcsEvent;
+import com.hypixel.hytale.component.system.EntityEventSystem;
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+import org.checkerframework.checker.nullness.compatqual.NullableDecl;
+
+public class PlayerSpawnSystem  {
+
+}
